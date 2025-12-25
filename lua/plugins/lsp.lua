bf451c08cc606c5781c5e489e9342941f2169784
may-z <mayureshsingh338@gmail.com>
2025-12-25 22:47:16 +0530

    configured and made some changes, more changes incoming

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,37 +2,40 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    event = "BufReadPre",
     config = function()
-      -- Configure LSP servers directly
-      local lspconfig = require("lspconfig")
-      
-      -- Lua
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-      })
-      
-      -- Python
-      lspconfig.pyright.setup({})
-      
-      -- TypeScript/JavaScript
-      lspconfig.tsserver.setup({})
-      
-      -- Add any other servers you need
-      
-      -- Key bindings (optional)
+      -- Key bindings on LSP attach
       local on_attach = function(client, bufnr)
         -- Key mappings
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
-        -- Add more as needed
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover" })
       end
-    end
-  }
+
+      -- Configure servers using vim.lsp.config (modern 0.11+ API)
+      local servers = {
+        lua_ls = {
+          on_attach = on_attach,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }
+              }
+            }
+          }
+        },
+        pyright = {
+          on_attach = on_attach,
+        },
+        ts_ls = {
+          on_attach = on_attach,
+        },
+      }
+
+      for server, config in pairs(servers) do
+        vim.lsp.config(server, config)
+      end
+      vim.lsp.enable(vim.tbl_keys(servers))
+    end,
+  },
 }
 
